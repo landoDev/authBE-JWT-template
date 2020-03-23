30ae8d6f78d4835001c681fd5225a2f9ba355603
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-23 12:23:42 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
 
 const usersRouter = require("../users/users-router.js");
 const authRouter = require("../auth/auth-router.js");
@@ -19,13 +20,13 @@ const sessionConfig = {
   // we should only save sessions when user allows it
   resave: false,
   saveUninitialized: false,
-  // store: new KnexSessionStore({
-  //   knex: require('../database/dbConfig.js'), // configured instance of knex
-  //   tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
-  //   sidfieldname: 'sid', // column that will hold the session id, name it anything you want
-  //   createtable: true, // if the table does not exist, it will create it automatically
-  //   clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
-  // }),
+  store: new KnexSessionStore({
+    knex: require('../database/dbConfig.js'), // configured instance of knex
+    tablename: 'sessions', // table that will store sessions inside the db, name it anything you want
+    sidfieldname: 'sid', // column that will hold the session id, name it anything you want
+    createtable: true, // if the table does not exist, it will create it automatically
+    clearInterval: 1000 * 60 * 60, // time it takes to check for old sessions and remove them from the database to keep it clean and performant
+  }),
 }
 
 server.use(session(sessionConfig))
