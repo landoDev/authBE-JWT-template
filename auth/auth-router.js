335fa849fffeea9c59edbd4eca79c9d8da753e5d
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-23 10:32:33 +0100

    wip

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,16 +1,16 @@
 const router = require("express").Router()
-const Users = require("./users-model.js")
+const Users = require("../users/users-model.js")
 
 router.get("/register", (req, res) => {
-
+  console.log('register endpoint')
 })
 
 router.get("/login", (req, res) => {
-
+  console.log('login endpoint')
 })
 
 router.get("/logout", (req, res) => {
-
+  console.log('logout endpoint')
 })
 
 module.exports = router
