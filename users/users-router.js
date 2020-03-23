4099bbef5dd4aa67f097898b12b42ad52f0bca16
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-23 12:09:46 +0100

    wip

--- users/users-router.js
+++ users/users-router.js
@@ -2,7 +2,15 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 
-router.get("/", (req, res) => {
+function protected(req, res, next) {
+  if (req.session && req.session.user) {
+    next()
+  } else {
+    res.status(401).json({ message: 'you have no session here' })
+  }
+}
+
+router.get("/", protected, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
