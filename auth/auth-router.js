5510be154be3d1f526eb257b5c925d37df2f14ac
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-03-23 10:41:45 +0100

    wip

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,9 +1,17 @@
+const bc = require('bcryptjs')
 const router = require("express").Router()
 const Users = require("../users/users-model.js")
 
 router.post("/register", (req, res) => {
   console.log('register endpoint')
-  
+  // 1- pull credentials from body
+  const { username, password } = req.body
+  // 2- make a hash using bcrypt
+  //        - import the lib
+  //        - use the lib
+  const hashedPassword = bc.hashSync()
+  // 3- we will save { username, password (hashed) } into db
+  // 4- we can json back to the client whatever, res.end, res.send, res.json
 })
 
 router.post("/login", (req, res) => {
